c6715717ba4dda9207599dfe98df91a4c2233cab
sbx_user1051 <sbx_user1051@169.254.102.1>
2022-05-19 17:21:08 +0000

    Thu May 19 17:21:08 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,21 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+items = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+    'E': 40
+}
+
+
+def get_remainder(value, divisor) -> int | int:
+    if value == 0:
+        return 0, 0
+    return value // divisor, value % divisor
+
+
 def discount_a(skus) -> int:
     return 0
 
@@ -15,17 +30,21 @@ def discount_e(skus) -> int:
 
 
 def apply_offers(skus) -> int:
-    return 0
+    return discount_a(skus) + discount_b(skus) + discount_e(skus)
 
 
 def checkout(skus):
     if len(skus) == 0:
         return 0
     if all(c in "ABCD" for c in skus):
-        a = skus.count('A') * 50 if 0 < skus.count('A') < 3 else (skus.count('A') // 3 * 130) + (skus.count('A') % 3 * 50)
-        b = skus.count('B') * 30 if 0 < skus.count('B') < 2 else (skus.count('B') // 2 * 45) + (skus.count('B') % 2 * 30)
-        c = skus.count('C') * 20
-        d = skus.count('D') * 15
-        return a + b + c + d
+        # a = skus.count('A') * 50 if 0 < skus.count('A') < 3 else (skus.count('A') // 3 * 130) + (skus.count('A') % 3 * 50)
+        # b = skus.count('B') * 30 if 0 < skus.count('B') < 2 else (skus.count('B') // 2 * 45) + (skus.count('B') % 2 * 30)
+        # c = skus.count('C') * 20
+        # d = skus.count('D') * 15
+        total = 0
+        for c in skus:
+            total += items[c]
+        discount = apply_offers(skus)
+        return total - discount
     else:
         return -1
