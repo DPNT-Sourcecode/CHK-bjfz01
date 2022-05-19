67054169d125f359f25de649f146558f7fc02efd
sbx_user1051 <sbx_user1051@169.254.137.21>
2022-05-19 15:09:19 +0000

    Thu May 19 15:09:19 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,13 +6,14 @@ def checkout(skus):
     if len(skus) == 0:
         return 0
     if all(c in "ABCD" for c in skus):
-        a = 50 if 0 < skus.count('A') < 3 else skus.count('A') * 50
-        b = 30 if 0 < skus.count('B') < 2 else skus.count('B') * 30
+        a = skus.count('A') * 50 if 0 < skus.count('A') < 3 else skus.count('A') * 50
+        b = skus.count('B') * 30 if 0 < skus.count('B') < 2 else skus.count('B') * 30
         c = skus.count('C') * 20
         d = skus.count('D') * 15
         return a + b + c + d
     else:
         return -1
+
 
 
 
